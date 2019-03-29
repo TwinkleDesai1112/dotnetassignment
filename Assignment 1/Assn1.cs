217038f17df8b13a068f8009561b695fd85338fa
TwinkleDesai1112 <46439100+TwinkleDesai1112@users.noreply.github.com>
2019-03-29 23:09:59 +0530

    Add files via upload

---
+++ Assignment 1/Assn1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Assn1
+{
+    public class Assn1
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("First Program");
+	    int i;
+	    i=25;
+        }
+    }
+}
